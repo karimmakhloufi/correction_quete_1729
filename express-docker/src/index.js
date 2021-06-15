c469406a0985abed879409facd44f9d9120eb4f0
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-06-15 16:53:53 +0200

    Changed the mongodb url to use docker container

--- express-docker/src/index.js
+++ express-docker/src/index.js
@@ -1,37 +1,40 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const MessageModel = require("./MessageModel")
+const express = require("express");
+const mongoose = require("mongoose");
+const MessageModel = require("./MessageModel");
 
 process.on("SIGINT", () => {
-    console.log("Caught interrupt signal");
-    process.exit();
+  console.log("Caught interrupt signal");
+  process.exit();
 });
 
 process.on("SIGTERM", () => {
-    console.log("Caught interrupt signal");
-    process.exit();
+  console.log("Caught interrupt signal");
+  process.exit();
 });
 
 (async () => {
-    const app = express();
+  const app = express();
 
-    console.log("Connecting to MongoDB");
-    await mongoose.connect("mongodb://127.0.0.1:27017/tom", { useUnifiedTopology: true, useNewUrlParser: true });
-    console.log("Connected");
+  console.log("Connecting to MongoDB");
+  await mongoose.connect("mongodb://mongodb:27017/tom", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  console.log("Connected");
 
-    app.get('/', (req, res) => {
-        console.log("Got a request");
-        res.json({ message: "Hey, I'm Tom, the API" });
-    });
+  app.get("/", (req, res) => {
+    console.log("Got a request");
+    res.json({ message: "Hey, I'm Tom, the API" });
+  });
 
-    app.post('/',  async (req, res) => {
-        console.log("Got a post");
-        const message = new MessageModel({sentence: "Hey I'm a new post!"});
-        const result = await message.save();
-        res.json({ message: "Hey, I saved a post", result });
-    })
+  app.post("/", async (req, res) => {
+    console.log("Got a post");
+    const message = new MessageModel({ sentence: "Hey I'm a new post!" });
+    const result = await message.save();
+    res.json({ message: "Hey, I saved a post", result });
+  });
 
-    app.listen(5000, () => {
-        console.log("Server is running");
-    });
+  app.listen(5000, () => {
+    console.log("Server is running");
+  });
 })();
